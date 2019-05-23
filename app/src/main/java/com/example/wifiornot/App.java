cdcb445869b5575a71d3a840fa57eecabd24d31f
Christian Malpass <chriscellphonerepair@gmail.com>
2019-05-23 16:26:07 -0400

    combined on boot service and running in foreground into example service

--- app/src/main/java/com/example/wifiornot/App.java
+++ app/src/main/java/com/example/wifiornot/App.java
@@ -20,7 +20,7 @@ public class App extends Application {
             NotificationChannel serviceChannel = new NotificationChannel(
                     CHANNEL_ID,
                     "EXAMPLE sERVICE cHANNEL",
-                    NotificationManager.IMPORTANCE_DEFAULT
+                    NotificationManager.IMPORTANCE_LOW
             );
 
             NotificationManager manager = getSystemService(NotificationManager.class);
--- app/src/main/java/com/example/wifiornot/ExampleService.java
+++ app/src/main/java/com/example/wifiornot/ExampleService.java
@@ -2,13 +2,22 @@ package com.example.wifiornot;
 
 import android.app.Notification;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static com.example.wifiornot.App.CHANNEL_ID;
 
 public class ExampleService extends Service {
+    private String notificationText = "You are not on Wifi";
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -16,15 +25,12 @@ public class ExampleService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle
-                ("example serivce").setContentTitle("example service")
-                .setContentText("hello")
-                .setSmallIcon(R.drawable.ic_android)
-                .build();
+        displayForeground();
+        Timer timer = new Timer();
+        TimerTask task = new ExampleService.Helper();
 
 
-        startForeground(1, notification);
+        timer.schedule(task, 0, 1000);
         return START_NOT_STICKY;
 
     }
@@ -33,4 +39,38 @@ public class ExampleService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+    public void displayForeground() {
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        if (mWifi.isConnected()) {
+            // Do whatever
+            notificationText = "You are on wifi";
+        } else {
+            notificationText = "You are not on wifi";
+        }
+
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle
+                ("WifiOrNot")
+                .setContentText(notificationText)
+                .setSmallIcon(R.drawable.ic_android)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .build();
+
+
+        startForeground(1, notification);
+    }
+
+    class Helper extends TimerTask {
+        public int i = 0;
+
+        public void run() {
+            System.out.println("hello world");
+            displayForeground();
+        }
+
+    }
 }
+
